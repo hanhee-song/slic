0f6c7a51abfc678d8d89c0a2b3437c295c310476
Hanhee Song <song.hanhee@gmail.com>
2017-10-22 18:31:45 -0400

    Initial npm setup

---
+++ frontend/slic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+
+// add test imports below
+
+document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});

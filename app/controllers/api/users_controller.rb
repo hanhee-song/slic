9ace0879f4450fbbe9d67baea9ef2914cc265408
Hanhee Song <song.hanhee@gmail.com>
2017-10-23 15:39:50 -0400

    add user new controller and jbuilder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,12 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   def update
@@ -9,5 +16,11 @@ class Api::UsersController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end

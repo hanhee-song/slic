fdb42bfd2d48ba32c932611fae01b5d4bf132ef8
Hanhee Song <song.hanhee@gmail.com>
2017-10-24 13:30:56 -0400

    add channel API utils

---
+++ frontend/util/channel_api_util.js
@@ -0,0 +1,36 @@
+export const fetchChannels = () => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/channels`,
+  });
+};
+
+export const fetchChannel = (channelId) => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/channels/${channelId}`,
+  });
+};
+
+export const createChannel = (channel) => {
+  return $.ajax({
+    method: "POST",
+    url: `/api/channels/`,
+    data: { channel },
+  });
+};
+
+export const updateChannel = (channel) => {
+  return $.ajax({
+    method: "PATCH",
+    url: `/api/channels/${channel.id}`,
+    data: { channel },
+  });
+};
+
+export const deleteChannel = (channelId) => {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/channels/${channelId}`,
+  });
+};

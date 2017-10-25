b50832ffb43eb6bfb64a6fcff60e8f988f7d1ec3
Hanhee Song <song.hanhee@gmail.com>
2017-10-24 22:46:18 -0400

    style new channel form

--- frontend/components/channel/channel_form.jsx
+++ frontend/components/channel/channel_form.jsx
@@ -29,23 +29,32 @@ class ChannelForm extends React.Component {
   
   render () {
     return (
-      <div className="channel-form-container">
-        <div
-          className="channel-form-new">
-          Channel Form
-          <form onSubmit={this.handleSubmit}>
-            <div onClick={this.handleClose}>
-              x
-            </div>
+      <div className="channel-form-absolute">
+        <div className="channel-form-container">
+          <div className="channel-form-header">
+            Create a new Channel
+          </div>
+          <i
+            className="channel-form-x fa fa-times"
+            aria-hidden="true"
+            onClick={this.handleClose}>
+          </i>
+          <form
+            className="channel-form-new"
+            onSubmit={this.handleSubmit}>
             <input
+              className="channel-form-input"
               type="text"
               onChange={this.handleChange("name")}
-              value={this.state.name} />
-            <input
-              type="text"
+              value={this.state.name}
+              placeholder="name" />
+            <textarea
+              className="channel-form-input"
               onChange={this.handleChange("description")}
-              value={this.state.description} />
+              value={this.state.description}
+              placeholder="description" />
             <input
+              className="channel-form-submit"
               type="submit"
               value="Create Channel"/>
           </form>
---
+++ todo_list.md
@@ -0,0 +1,3 @@
+* Implement login via both username and email
+  * change User.find_by_credentials to accommodate emails
+  * login input should say "username or email"

9671b5acbf3a0d5b5d363e67c48123f28c071c1c
Hanhee Song <song.hanhee@gmail.com>
2017-10-27 09:34:10 -0400

    add chat styling and chat react skeleton

---
+++ frontend/components/chat_header/chat_header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ChatHeader extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render () {
+    return (
+      <div>this is a chat header</div>
+    );
+  }
+}
+
+export default ChatHeader;

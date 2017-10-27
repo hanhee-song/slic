f90573b1a40a7a3ae13dc8990ea5094ac65ea90c
Hanhee Song <song.hanhee@gmail.com>
2017-10-27 09:54:46 -0400

    slim down chat header and styling (prev commit is buggy)

--- frontend/components/chat_header/chat_header.jsx
+++ frontend/components/chat_header/chat_header.jsx
@@ -7,7 +7,27 @@ class ChatHeader extends React.Component {
   
   render () {
     return (
-      <div>this is a chat header</div>
+      <div class="chat-header">
+        <div class="chat-header-left">
+          <div class="chat-header-left-title button">
+            # Temporary title
+          </div>
+          <div class="chat-header-left-options">
+            <div class="chat-header-left-options users button">
+              <i class="fa fa-user-o" aria-hidden="true"></i> 71
+            </div>
+          </div>
+        </div>
+        
+        <div class="chat-header-right">
+          <div class="chat-header-right-options info button">
+            <i class="fa fa-info-circle" aria-hidden="true"></i>
+          </div>
+          <div class="chat-header-right-options settings button">
+            <i class="fa fa-cog" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- frontend/components/workspace/workspace.jsx
+++ frontend/components/workspace/workspace.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Sidebar from '../sidebar/sidebar';
+import ChatHeaderContainer from '../chat_header/chat_header_container';
 
 const Workspace = () => (
   <div className="workspace">
     <Sidebar />
+    <div className="chat">
+      <ChatHeaderContainer />
+    </div>
   </div>
 );
 

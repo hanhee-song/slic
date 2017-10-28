42221682fc57789741e814c72b7a3526d7190b4c
Hanhee Song <song.hanhee@gmail.com>
2017-10-28 18:59:12 -0400

    add message error reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_errors_reducer';
 import ChannelErrorsReducer from './channel_errors_reducer';
+import MessageErrorsReducer from './message_errors_reducer';
 
 const ErrorsReducer = combineReducers({
   session: SessionErrorsReducer,
   channel: ChannelErrorsReducer,
+  message: MessageErrorsReducer,
 });
 
 export default ErrorsReducer;

df9469ddde08bb65697c610d4860f532e11df796
Hanhee Song <song.hanhee@gmail.com>
2017-11-01 17:34:21 -0400

    remove debugger for push

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -70,7 +70,6 @@ class Api::ChannelsController < ApplicationController
     
     @channel = Channel.new(channel_params)
     if @channel.save
-      debugger
       option_params[:user_ids].each do |id|
         @channel.subscriptions.create!(
           user_id: id,

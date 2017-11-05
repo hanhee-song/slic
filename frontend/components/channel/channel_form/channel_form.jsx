cad9911d4fd71816035cb87083dc339c49cf5324
Hanhee Song <song.hanhee@gmail.com>
2017-11-04 22:29:24 -0400

    Temporary fix for channel errors showing up in form upon leaving private channel

--- frontend/components/channel/channel_form/channel_form.jsx
+++ frontend/components/channel/channel_form/channel_form.jsx
@@ -16,6 +16,7 @@ class ChannelForm extends React.Component {
   }
   
   componentDidMount() {
+    this.props.clearChannelErrors();
     document.addEventListener("keydown", this.handleEscape, false);
   }
   
@@ -45,8 +46,6 @@ class ChannelForm extends React.Component {
           const id = response.channel.id;
           const user_id = this.props.currentUser.id;
           this.props.subscribeUserIdsToChannel(response.channel, [this.props.currentUser.id]);
-          // this.props.rememberCurrentChannelId(
-          //   this.props.currentUser, response.channel.id);
           this.handleClose();
           this.props.history.push(`/channels/${response.channel.id}`);
         }

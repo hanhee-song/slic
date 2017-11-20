403040f83a1fb1b761d05415044847c9f5e5038b
Hanhee Song <song.hanhee@gmail.com>
2017-11-20 13:44:55 -0500

    update email

--- frontend/components/channel/channel_sidebar/channel_sidebar_footer.jsx
+++ frontend/components/channel/channel_sidebar/channel_sidebar_footer.jsx
@@ -5,13 +5,13 @@ const ChannelSidebarFooter = () => (
     <div className="sidebar-footer-text">
       Hanhee Song
       <br />
-      hsong01@wesleyan.edu
+      song.hanhee@gmail.com
     </div>
     <div className="sidebar-footer-icons">
       <a href="https://hanhee-song.com" target="_blank"><i className="fa fa-home" aria-hidden="true"></i></a>
       <a href="https://github.com/hanhee-song/slic" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
       <a href="https://www.linkedin.com/in/hanhee-song" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
-      <a href="mailto:hsong01@wesleyan.edu" className="fa fa-envelope-o"></a>
+      <a href="mailto:song.hanhee@gmail.com" className="fa fa-envelope-o"></a>
     </div>
   </div>
 );
